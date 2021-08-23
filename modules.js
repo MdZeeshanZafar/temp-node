8d89bf81a41dc268f284cd2d5774d934213b9dce
Zeeshan <zeeshanzafar2012@gmail.com>
2021-08-24 03:14:03 +0530

    first commit

---
+++ modules.js
@@ -0,0 +1,7 @@
+const names = require('./names');
+const sayHi = require('./utils')
+const data = require('./alter')
+require('./mindGrenade')
+sayHi('Zeeshan')
+sayHi(names.john)
+sayHi(names.peter)
